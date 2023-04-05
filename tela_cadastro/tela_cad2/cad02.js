832d8936b0e2a755a590f1efc96780ac12673b53
thiagoluz959@gmail.com <thiagoluz959@gmail.com>
2023-04-05 14:18:54 -0300

    validacao corrigida

--- tela_cadastro/tela_cad2/cad02.js
+++ tela_cadastro/tela_cad2/cad02.js
@@ -5,36 +5,36 @@ const numero = document.querySelector('#number');
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
     //verifica cpf
-    if(CPF.value === '' || !validaCPF(CPF.value)){
+    if(CPF.value === '' || !validaCPF(cpf.value)){
         alert("Insira o CPF corretamente, por favor")
         return;
     }
 
-    if(numero.value === '' || !validaNumero(numero.value)){
+    if(numero.value === '' || !validaNumero(number.value)){
         alert("Insira o numero corretamente")
         return;
     }
 });
 
 //função valida cpf
-function validaCPF(CPF){
+function validaCPF(cpf){
     const cpfRegex = new RegExp(
         //xxx.xxx.xxx-xx
         /^[0-9]{11,}$/
     );
-    if(cpfRegex.test(CPF)){
+    if(cpfRegex.test(cpf)){
         return true;
     }else{
         return false;
     }
 }
 //função valida numero
-function validaNumero(numero){
+function validaNumero(number){
     const numberRegex = new RegExp(
         //xxxxxxxxx
     /^[0-9]{9,}$/
     );
-    if(numberRegex.test(numero)){
+    if(numberRegex.test(number)){
         return true;
     }else{
         return false;
