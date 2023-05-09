0a1135c44080810e29f7818f32cabe1bdfd9a54c
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-05-08 23:51:07 -0300

    arrumando commits

--- tela_cadastro/cad01.js
+++ tela_cadastro/cad01.js
@@ -1,8 +1,15 @@
+console.log("Oi");
+alert('oieee111');
+
 const container = document.querySelector('.container');
-const name = document.querySelector('#nome');
-const email = document.querySelector('#mail');
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
 const senha = document.querySelector('#password');
 const Confpassword = document.querySelector('#_confirmePassword');
+const form = document.querySelector('#form');
+const CPF = document.querySelector('#cpf');
+const numero = document.querySelector('#number');
+const mes = document.querySelector('#month')
 
 
 form.addEventListener("submit", (e) =>{
@@ -16,7 +23,7 @@ form.addEventListener("submit", (e) =>{
     }
     //valida email(beta)
     
-    if(email.value === "" || !isEmailValido(mail.value)){
+    if(email.value === "" || !isEmailValido(email.value)){
         error(email, "Insira um email válido!");
         return;
     }else{
@@ -24,7 +31,7 @@ form.addEventListener("submit", (e) =>{
     }
 
     //verifica a senha(beta)
-    if(senha.value === "" || !validaPassword(password.value)){
+    if(senha.value === "" || !validaPassword(senha.value)){
         error(senha, "A senha precisa de 5 caracteres, sendo um numero e um caracter maiusculo");
         return;
     }else{
@@ -32,16 +39,23 @@ form.addEventListener("submit", (e) =>{
     }
 
     //confirma a senha(beta)
-    if(Confpassword.value === "" || confirmaPassword(_confirmePassword.value) != validaPassword(password.value)){
+    if(Confpassword.value === "" || confirmaPassword(Confpassword.value) != validaPassword(password.value)){
         error(Confpassword, "As senhas não são iguais");
         return;
     }
-    else if (!validaPassword(password.value)) {
+    else if (!validaPassword(senha.value)) {
         error(senha, "A senha precisa de 5 caracteres, sendo um numero e um caracter maiusculo");
         return;
-    } else {
-        success(senha)
-        window.location = "/tela_cadastro/tela_cad2/index.html";
+    } if(CPF.value === '' || !validaCPF(CPF.value)){
+        error(CPF, "Insira o CPF corretamente, por favor")
+        return;
+    }else{
+        success(CPF);
+    }
+
+    if(numero.value === '' || !validaNumero(numero.value)){
+        error(numero, "Insira o numero corretamente")
+        return;
     }
 });
 
@@ -58,12 +72,12 @@ function isNomeValido(nome){
 }
 
 //Funcao valida email
-function isEmailValido(mail){
+function isEmailValido(email){
     const emailRegex = new RegExp(
         //usario@host.com e etc
         /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
     );
-    if (emailRegex.test(mail)){
+    if (emailRegex.test(email)){
         return true;
     }else{
         return false;
@@ -71,21 +85,46 @@ function isEmailValido(mail){
 }
 
 //Funcao valida senha(não é a confirmação)
-function validaPassword(password){
+function validaPassword(senha){
     const senhaRegex = new RegExp(
         /^(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{5,}$/
     );
-    if (senhaRegex.test(password)){
+    if (senhaRegex.test(senha)){
         return true;
     }else{
         return false;
     }
 }
-function confirmaPassword(_confirmePassword){
+function confirmaPassword(Confpassword){
     const senhaConfRegex = new RegExp(
         /^(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{5,}$/
     );
-    if (senhaConfRegex.test(_confirmePassword)){
+    if (senhaConfRegex.test(Confpassword)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//função valida cpf
+function validaCPF(CPF){
+    const cpfRegex = new RegExp(
+        //xxx.xxx.xxx-xx ou xxxxxxxxxxx
+        /^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}/
+    );
+    if(cpfRegex.test(CPF)){
+        return true;
+    }else{
+        return false;
+    }
+}
+//função valida numero
+function validaNumero(numero){
+    const numberRegex = new RegExp(
+        //(xx) xxxxx-xxxx
+    /^[0-9]{11}$/
+    );
+    if(numberRegex.test(numero)){
         return true;
     }else{
         return false;
@@ -93,6 +132,23 @@ function confirmaPassword(_confirmePassword){
 }
 
 //mensagem de erro
+function error(element, msg){
+    element.style.border = '3px red solid'
+    const parent = element.parentElement;
+    const p = parent.querySelector('p');
+    p.textContent = msg;
+    p.style.visibility = 'visible';
+}
+
+function success(element){
+    element.style.border = '3px green solid'
+    const parent = element.parentElement;
+    const p = parent.querySelector('p');
+    p.style.visibility = 'hidden';
+}
+
+
+//mensagem de alerta
 function error(element, msg){
     element.style.border = '3px red solid'
     const parent = element.parentElement;
