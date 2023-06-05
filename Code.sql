d008b82bdf50cea75562e5dad31625378753664d
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-06-05 13:46:49 -0300

    Create Code.sql

---
+++ Code.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE l2w;
+##
+USE teste;
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT,
+  nome VARCHAR(150),
+  email VARCHAR(150),
+  senha VARCHAR(150),
+  cpf VARCHAR(11),
+  numTel VARCHAR(11),
+  data_nascimento DATE,
+  PRIMARY KEY (id)
+);
+###
+USE teste;
+CREATE TABLE video (
+  id INT AUTO_INCREMENT,
+  titulo VARCHAR(400),
+  descricao VARCHAR(400),
+  nome_arquivo VARCHAR(400),
+  PRIMARY KEY (id)
+);
+###
+USE teste;
+CREATE TABLE historico (
+  id INT AUTO_INCREMENT,
+  user_id INT,
+  video_id int,
+  video_titulo VARCHAR(400),
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES usuarios(id),
+  FOREIGN KEY (video_id) REFERENCES video(id)
+);
+####
+USE teste;
+CREATE TABLE avaliacao (
+  id_user int,
+  nota int,
+  FOREIGN KEY (id_user ) REFERENCES usuarios(id)
+);
+#####
+USE teste;
+create table adm(
+  id INT AUTO_INCREMENT,
+  nome VARCHAR(150),
+  email VARCHAR(150),
+  senha VARCHAR(150),
+  PRIMARY KEY (id)
+);
+
