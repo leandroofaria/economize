01d4af79c40a0485c389f568c28ee254d7d17d14
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-06-07 15:48:26 -0300

    Update Code.sql

--- Code.sql
+++ Code.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE l2w;
 ##
-USE teste;
+USE l2w;
 CREATE TABLE usuarios (
   id INT AUTO_INCREMENT,
   nome VARCHAR(150),
@@ -13,7 +13,7 @@ CREATE TABLE usuarios (
   PRIMARY KEY (id)
 );
 ###
-USE teste;
+USE l2w;
 CREATE TABLE video (
   id INT AUTO_INCREMENT,
   titulo VARCHAR(400),
@@ -22,23 +22,22 @@ CREATE TABLE video (
   PRIMARY KEY (id)
 );
 ###
-USE teste;
+USE l2w;
 CREATE TABLE historico (
   id INT AUTO_INCREMENT,
   user_id INT,
-  video_id int,
-  video_titulo VARCHAR(400),
+  video_titulo int VARCHAR(400),
+  video_url VARCHAR(400),
   PRIMARY KEY (id)
 );
 ####
-USE teste;
+USE l2w;
 CREATE TABLE avaliacao (
   id_user int,
-  nota int,
-  FOREIGN KEY (id_user ) REFERENCES usuarios(id)
+  nota int
 );
 #####
-USE teste;
+USE l2w;
 create table adm(
   id INT AUTO_INCREMENT,
   nome VARCHAR(150),
